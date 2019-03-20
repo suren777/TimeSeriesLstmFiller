39327f32716ea58f4142d8b2c0a264db4cc7c0a8
si777 <suren.islyaev@gmail.com>
2019-03-20 23:53:54 +0000

    Update

--- CODE/models.py
+++ CODE/models.py
@@ -2,21 +2,33 @@ import tensorflow.keras as kf
 
 def create_2f_model(n_units, input_shape, file=None):
     window_size,input_dim = input_shape
-    input_1 = kf.layers.Input(shape=(window_size,input_dim-1))
+    input_1 = kf.layers.Input(shape=(window_size, input_dim - 1))
     input_2 = kf.layers.Input(shape=(window_size - 1, 1))
 
-    lstm_1= kf.layers.LSTM(n_units, return_state=True)
-    output,  output_h, output_c = lstm_1(input_1)
-    encoder_states = [output_h, output_c]
+    # lstm_1= kf.layers.LSTM(n_units, return_state=True)
+    # output,  output_h, output_c = lstm_1(input_1)
 
-    lstm_2 = kf.layers.LSTM(n_units)
+    lstm_1 = kf.layers.Bidirectional(kf.layers.CuDNNLSTM(n_units, return_state=True))
+    #lstm_1 = kf.layers.Bidirectional(kf.layers.LSTM(n_units, return_state=True))
+    _, output_h1, output_c1, output_h2, output_c2 = lstm_1(input_1)
+    output_h = kf.layers.Concatenate()([output_h1, output_h2])
+    output_c = kf.layers.Concatenate()([output_c1, output_c2])
+
+    dense_h = kf.layers.Dense(n_units,activation='tanh')(output_h)
+    dense_C = kf.layers.Dense(n_units,activation='tanh')(output_c)
+
+    encoder_states = [dense_h, dense_C]
+
+    lstm_2 = kf.layers.CuDNNLSTM(n_units)
+    #lstm_2 = kf.layers.LSTM(n_units)
     lstm_out = lstm_2(input_2, initial_state=encoder_states)
 
-    dense_in = kf.layers.Dense(n_units//2,activation='tanh')(lstm_out)
-    dense_out = kf.layers.Dense(1, activation='linear', use_bias=False)(dense_in)
+    dense_in = kf.layers.Dense(n_units // 2, activation='tanh')(lstm_out)
+    dense_out = kf.layers.Dense(1)(dense_in)
 
     model = kf.models.Model(inputs=[input_1, input_2], outputs=[dense_out])
-    model.compile(optimizer='rmsprop', loss='mse')
+    optimizer = kf.optimizers.RMSprop(lr=0.001, clipnorm=5)
+    model.compile(optimizer=optimizer, loss='mse')
 
     if file == file:
         try:
--- CODE/two_factor_lstm.py
+++ CODE/two_factor_lstm.py
@@ -17,10 +17,11 @@ print(diff_df.head())
 X1, X2, Y = generate_windows_for_two_factor(diff_df, window_size)
 
 file = 'FILES/Models/lstm_2f_model.hdf5'
-input_shape = (window_size,df.values.shape[1])
+rf_num = diff_df.values.shape[1]
+input_shape = (window_size,rf_num)
 model = create_2f_model(n_units, input_shape, file)
 
-epochs = 0
+epochs = 100
 for i in range(epochs):
     hist = model.fit(x=[X1, X2],
               y=Y,
@@ -30,6 +31,9 @@ for i in range(epochs):
               validation_split=0.05,
               callbacks=[])
     if i > 0:
+        if hist.history['val_loss'][-1] !=  hist.history['val_loss'][-1]:
+            print('NaN loss exiting')
+            break
         if hist.history['val_loss'][-1]<val_loss:
             val_loss = hist.history['val_loss'][-1]
             print("New val_loss: {}\t epoch: {}".format(val_loss,i))
@@ -38,15 +42,18 @@ for i in range(epochs):
         val_loss =  hist.history['val_loss'][-1]
 
 start = 100
+window = 100
 test_df = df.iloc[0:300,0].to_frame()
-test_df['GAP'] = test_df.iloc[:,0]
-test_df['GAP'].iloc[start:start+100]=np.nan
+test_df.columns=['Actual']
+test_df['Interp'] = test_df.iloc[:,0]
+test_df['Interp'].iloc[start:start+window]=np.nan
 test_df = test_df.interpolate()
+start_value = test_df.iloc[start-1,0]
+end_value = test_df.iloc[start+window,0]
+
 
 Y_pred, Y_actual=list(), list()
-
-
-for id in range(start,start+100):
+for id in range(start, start+window):
     if id == start:
         x2 = X2[id].reshape(-1, window_size - 1, 1)
     x1 = X1[id].reshape(-1,window_size,input_shape[1]-1)
@@ -56,5 +63,35 @@ for id in range(start,start+100):
     x2[0,-1,0] = Y_pred[-1]
 
 test_df['Predicted']=np.nan
+fixed_values = np.array(Y_pred)+1
+for i, value in enumerate(fixed_values):
+    if i == 0:
+        fixed_values[0]=value*start_value
+    else:
+        fixed_values[i]=fixed_values[i-1]*value
+multiplier = end_value/fixed_values[-1]
+singlemultipliers = np.array([multiplier**(x/(window+1)) for x in range(window+2)])
+#singlemultipliers = np.array([multiplier**(0) for x in range(window+2)])
+test_df['Predicted'].iloc[start:start+100]=fixed_values[:, 0]*singlemultipliers[:-2, 0]
+test_df.plot()
+
+test_df['Predicted']=np.nan
 test_df['Predicted'].iloc[start:start+100]=Y_pred
 test_df.plot()
+
+compare_df = test_df.dropna().copy()
+compare_df['LinInterpDiffs']=abs(compare_df['Actual']-compare_df['Interp'])/compare_df['Actual']
+compare_df['LstmPredDiffs']=abs(compare_df['Actual']-compare_df['Predicted'])/compare_df['Actual']
+print(compare_df.head())
+
+compare_df.loc[:,['LinInterpDiffs', 'LstmPredDiffs'] ].plot()
+
+print("Lstm Average Error: {}".format(compare_df['LstmPredDiffs'].mean()))
+print("Interpolation Average Error: {}".format(compare_df['LinInterpDiffs'].mean()))
+
+from scipy import stats
+
+resLstm = stats.ks_2samp(compare_df['Actual'].values, compare_df['Predicted'].values)
+print("Lstm KS result: \n \t - Statistic: {0} \n \t - pvalue: {1}".format(resLstm.statistic, resLstm.pvalue))
+resInterp = stats.ks_2samp(compare_df['Actual'].values, compare_df['Interp'].values)
+print("Interp KS result: \n \t - Statistic: {0} \n \t - pvalue: {1}".format(resInterp.statistic, resInterp.pvalue))
